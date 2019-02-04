cd1a4460570ea6e240930bcc99e21002ce1783cc
DeNoctiss <46870670+DeNoctiss@users.noreply.github.com>
2019-02-04 13:16:42 +0200

    Add files via upload

---
+++ snakee/snakee/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace snakee
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Field = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.Field)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Field
+            // 
+            this.Field.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Field.Location = new System.Drawing.Point(0, 0);
+            this.Field.Name = "Field";
+            this.Field.Size = new System.Drawing.Size(349, 305);
+            this.Field.TabIndex = 0;
+            this.Field.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(349, 305);
+            this.Controls.Add(this.Field);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Field)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Field;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ snakee/snakee/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace snakee
+{
+    public partial class Form1 : Form
+    {
+        Snake snake;
+        public Form1()
+        {
+            InitializeComponent();
+            
+            Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Start()
+        {
+            snake = new Snake(Field);
+            snake.Draw();
+        }
+    }
+}
